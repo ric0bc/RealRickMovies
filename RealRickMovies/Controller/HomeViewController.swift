e37763599ca437d5e2414fd72f62046c21cf1673
Ricky <ricky@Development.local>
2017-11-08 17:44:55 +0100

    type: feat
    body: added tableview for movies

--- RealRickMovies/Controller/HomeViewController.swift
+++ RealRickMovies/Controller/HomeViewController.swift
@@ -8,28 +8,68 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITableViewController {
+    
+    var imageBackdrop: UIImage!
+    var arrayMovies: Array<[String:AnyObject]>!
+    var imageString: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Zeig \(Constants.sessionID) and \(Constants.apiKey)")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        getLatestMovie()
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMovies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as UITableViewCell!
+        cell?.imageView!.image = imageBackdrop
+//        let movie = array[(indexPath as NSIndexPath).row]
+//        cell?.textLabel?.text = movie["title"] as? String
+        return cell!
+    }
+    
+    
+    func getLatestMovie() {
+        let urlString = "https://api.themoviedb.org/3/movie/top_rated?language=de-CH&api_key=\(Constants.apiKey)&page=1"
+        let requestURL = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: requestURL!) {(data, reponse, error ) in
+            if error == nil {
+                if let data = data {
+                    let parsedResults: [String:AnyObject]!
+                    do {
+                        parsedResults = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+                        print(parsedResults)
+                        self.arrayMovies = parsedResults["results"] as? [[String:AnyObject]]
+                    } catch {
+                        print("An error with parsing")
+                        return
+                    }
+                }
+            } else {
+                print("there was an error: \(error!)")
+            }
+            self.getImage()
+        }
+        task.resume()
+    }
+    func getImage(){
+        let urlString = URL(string: "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg")
+        if let imageData = try? Data(contentsOf: urlString!) {
+            DispatchQueue.main.async {
+                self.imageBackdrop = UIImage(data: imageData)
+                self.tableView.reloadData()
+            }
+        } else {
+            print("error loading image")
+        }
+        
+    }
 }
---
+++ RealRickMovies/Helpers/TMDBMovie.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBMovie.swift
+//  RealRickMovies
+//
+//  Created by Ricky on 08.11.17.
+//  Copyright © 2017 Ricky. All rights reserved.
+//
+
+import Foundation
+
+struct TMDBMovie {
+    
+    let title: String
+    let id: Int
+    let imageName: String
+    
+}
